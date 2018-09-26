a26a4c857ab55f3b952a42366967ac99415527c0
ASG\gerardc <gerard.castello@asg.com>
2018-09-26 20:54:04 +0200

    adding some tests for draft-entry.component

--- src/frontend/src/app/draft-list/draft-entry/draft-entry.component.spec.ts
+++ src/frontend/src/app/draft-list/draft-entry/draft-entry.component.spec.ts
@@ -1,6 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
-import { RouterTestingModule } from '@angular/router/testing';
+import { RouterTestingModule, SpyNgModuleFactoryLoader } from '@angular/router/testing';
 import { ActivatedRoute, Params } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
@@ -10,7 +10,6 @@ import { Observable } from 'rxjs/Observable';
 import { DraftEntryComponent } from './draft-entry.component';
 import { httpLoaderFactory } from '../../app.module';
 import { BackendApiService } from '../../services/backend-api.service';
-import { IncrementalResponseDto } from '../../services/Dtos/IncrementalResponseDto';
 import { DraftComponent } from '../draft/draft.component';
 import { DraftFooterComponent } from '../draft-footer/draft-footer.component';
 import { StorageService } from '../../services/storage.service';
@@ -21,6 +20,10 @@ import { DataBuilder } from '../../testing/data.builder';
 describe('DraftEntryComponent', () => {
   let component: DraftEntryComponent;
   let fixture: ComponentFixture<DraftEntryComponent>;
+  let getDraftMock: any;
+  let setVisitMock: any;
+  let getDraftRelationshipMock: any;
+  let getDraftSuggestionsMock: any;
 
   const draft = DataBuilder.getDraftsDto()[0];
   const relatedDrafts = DataBuilder.getDraftDigestsDto().splice(0, 1);
@@ -28,7 +31,7 @@ describe('DraftEntryComponent', () => {
   const expressions = DataBuilder.getExpressions();
   const apiServiceMock = <BackendApiService>{
     getDraft: (id) => new Observable(observer => observer.next(draft)),
-    setVisit: (id) => new Observable(observer => observer.next(new IncrementalResponseDto())),
+    setVisit: (id) => new Observable(observer => observer.complete),
     getDraftRelationship: (id) => new Observable(observer => observer.next(relatedDrafts)),
     getDraftSuggestions: (id, take) => new Observable(observer => observer.next(suggestion))
   };
@@ -42,6 +45,11 @@ describe('DraftEntryComponent', () => {
   };
 
   beforeEach(async(() => {
+    getDraftMock = spyOn(apiServiceMock, 'getDraft').and.callThrough();
+    setVisitMock = spyOn(apiServiceMock, 'setVisit').and.callThrough();
+    getDraftRelationshipMock = spyOn(apiServiceMock, 'getDraftRelationship').and.callThrough();
+    getDraftSuggestionsMock = spyOn(apiServiceMock, 'getDraftSuggestions').and.callThrough();
+
     TestBed.configureTestingModule({
       declarations: [
         DraftEntryComponent,
@@ -85,5 +93,12 @@ describe('DraftEntryComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should initialize values for the requested draft', () => {
+    expect(getDraftMock).toHaveBeenCalled();
+    expect(setVisitMock).toHaveBeenCalled();
+    expect(getDraftRelationshipMock).toHaveBeenCalled();
+    expect(getDraftSuggestionsMock).toHaveBeenCalled();
   });
 });
