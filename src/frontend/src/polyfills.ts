75916b649723cee77a222d0e8c9a1d4c272e063c
gcastellov <dreeyfus20@hotmail.com>
2018-09-27 21:11:40 +0200

    adding Circle CI config file and fixing lint errors

--- src/frontend/src/polyfills.ts
+++ src/frontend/src/polyfills.ts
@@ -73,4 +73,4 @@ import 'rxjs/add/observable/throw';
 import 'rxjs/add/operator/filter';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/operator/take';
-import 'rxjs/add/operator/map'; 
+import 'rxjs/add/operator/map';
