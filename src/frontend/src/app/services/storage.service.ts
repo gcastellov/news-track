833930868b73d60691b4297a23b72d8e1a07c0b7
gcastellov <dreeyfus20@hotmail.com>
2018-09-28 20:09:29 +0200

    changing sessionStorage for localStorage at storage.service

--- src/frontend/src/app/services/storage.service.ts
+++ src/frontend/src/app/services/storage.service.ts
@@ -1,11 +1,11 @@
 export class StorageService {
 
     setItem(key: string, value: string) {
-        sessionStorage.setItem(key, value);
+        localStorage.setItem(key, value);
     }
 
     getItem(key: string): string {
-        return sessionStorage.getItem(key);
+        return localStorage.getItem(key);
     }
 
 }
