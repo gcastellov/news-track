a0820d79f4f5b349f4ad3ce49aed8a9f6a5d150c
gcastellov <dreeyfus20@hotmail.com>
2018-10-02 13:56:00 +0200

    adding some tests for change-password.component

--- src/frontend/src/app/membership/change-password/change-password.component.spec.ts
+++ src/frontend/src/app/membership/change-password/change-password.component.spec.ts
@@ -10,12 +10,16 @@ import { ChangePasswordComponent } from './change-password.component';
 import { httpLoaderFactory } from '../../app.module';
 import { TestBedHelper } from '../../testing/testbed.helper';
 import { BackendApiService } from '../../services/backend-api.service';
+import { Observable } from 'rxjs/Observable';
+import { ChangePasswordResponseDto } from '../../services/Dtos/ChangePasswordResponseDto';
 
 describe('ChangePasswordComponent', () => {
   let component: ChangePasswordComponent;
   let fixture: ComponentFixture<ChangePasswordComponent>;
 
-  const apiServiceMock = <BackendApiService>{};
+  const apiServiceMock = <BackendApiService>{
+    changePassword: (r) => new Observable<ChangePasswordResponseDto>(observer => observer.complete)
+  };
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -49,5 +53,49 @@ describe('ChangePasswordComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should change password', () => {
+    const currentPwd = 'MyCurrentPassword';
+    const newPwd = 'MyNewPwd';
+    component.pwdForm.controls['currentPassword'].setValue(currentPwd);
+    component.pwdForm.controls['password1'].setValue(newPwd);
+    component.pwdForm.controls['password2'].setValue(newPwd);
+
+    const responseDto = new ChangePasswordResponseDto();
+    responseDto.isSuccessful = true;
+
+    const changePasswordMock = spyOn(apiServiceMock, 'changePassword').and
+      .callFake(() => new Observable<ChangePasswordResponseDto>(o => o.next(responseDto)));
+
+    component.changePassword();
+
+    expect(changePasswordMock).toHaveBeenCalled();
+    expect(component.pwdForm.get('currentPassword').value).toBe('');
+    expect(component.pwdForm.get('password1').value).toBe('');
+    expect(component.pwdForm.get('password2').value).toBe('');
+  });
+
+  it('should show proper message when changing password fails', () => {
+    const currentPwd = 'MyCurrentPassword';
+    const newPwd = 'MyNewPwd';
+    component.pwdForm.controls['currentPassword'].setValue(currentPwd);
+    component.pwdForm.controls['password1'].setValue(newPwd);
+    component.pwdForm.controls['password2'].setValue(newPwd);
+
+    const responseDto = new ChangePasswordResponseDto();
+    responseDto.isSuccessful = false;
+    responseDto.failure = 1;
+
+    const changePasswordMock = spyOn(apiServiceMock, 'changePassword').and
+      .callFake(() => new Observable<ChangePasswordResponseDto>(o => o.next(responseDto)));
+
+    component.changePassword();
+
+    expect(changePasswordMock).toHaveBeenCalled();
+    expect(component.failureReason).toBe(responseDto.failure);
+    expect(component.pwdForm.get('currentPassword').value).toBe(currentPwd);
+    expect(component.pwdForm.get('password1').value).toBe(newPwd);
+    expect(component.pwdForm.get('password2').value).toBe(newPwd);
   });
 });
