d3cd171d0f482e72c9b89f15d715f08caefe0f5b
gcastellov <dreeyfus20@hotmail.com>
2021-12-06 21:40:21 +0100

    adding coverage for comment replies endpoint

---
+++ src/backend/NewsTrack.WebApi.IntegrationTests/CommentRepliesEndpointTests.cs
@@ -0,0 +1,69 @@
+﻿using FluentAssertions;
+using NewsTrack.WebApi.Dtos;
+using NewsTrack.WebApi.IntegrationTests.Fixture;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace NewsTrack.WebApi.IntegrationTests
+{
+    public class CommentRepliesEndpointTests : BaseTest
+    {
+        private const string Endpoint = "/api/comment/{commentId}/replies";
+        
+        public CommentRepliesEndpointTests(TestWebAppFactory<Startup> testWebAppFactory)
+            : base(testWebAppFactory)
+        {
+        }
+
+        [Fact]
+        public async Task GivenCommentAndReplies_WhenGettingReplies_ThenReturnsCollection()
+        {
+            // Arrange
+            const int page = 1;
+            const int count = 5;
+
+            var comment = CreateCommentEntity();
+            var replies = new[] { comment };
+            var pathAndQuery = GetUriWithQueryString(
+                Endpoint.Replace("{commentId}", comment.ReplyingTo.ToString()),
+                new Tuple<string, object>("page", page),
+                new Tuple<string, object>("count", count));
+
+            Factory.CommentRepositoryMock.Setup(m => m.GetReplies(comment.ReplyingTo.Value, page, count)).Returns(Task.FromResult(replies.AsEnumerable()));
+
+            // Act
+            var response = await Client.GetAsync(pathAndQuery);
+
+            // Assert
+            response.ShouldBeSuccessful();
+            var envelope = await response.ShouldBeOfType<IEnumerable<CommentDto>>();
+            envelope.ShouldBeSuccessful();
+
+            envelope.Payload.Should().HaveCount(replies.Length);
+            var dto = envelope.Payload.First();
+            AssertCommentDto(comment, dto);
+        }
+
+        [Fact]
+        public async Task GivenInvalidNonPositiveCount_WhenGettingReplies_GetsBadRequest()
+        {
+            // Arrange
+            const int page = 1;
+            const int count = 0;
+
+            var pathAndQuery = GetUriWithQueryString(
+                Endpoint.Replace("{commentId}", Guid.NewGuid().ToString()),
+                new Tuple<string, object>("page", page),
+                new Tuple<string, object>("count", count));
+
+            // Act
+            var response = await Client.GetAsync(pathAndQuery);
+
+            // Assert
+            response.ShouldBeBadRequest();
+        }
+    }
+}
